3b95ec6904b0de06ad4f2323cb733f00d4fb46e3
Patrick Bowen <phunanon@users.noreply.github.com>
2021-04-12 19:10:43 +0100

    Shrink everything Because if this thing isn't small it has basically 0 utility

--- pH7.ts
+++ pH7.ts
@@ -5,7 +5,6 @@ const isArr = Array.isArray;
 const isStr = (x: any): x is string => typeof x == "string";
 const isObj = (x: any): x is object => typeof x == "object";
 const isEvt = (x: any): x is DomEvent => typeof x == "function";
-const isUnd = (x: any): x is undefined => typeof x == "undefined";
 
 function hash(s: string): number {
   for (var i = 0, h = 9; i < s.length; )
@@ -22,48 +21,37 @@ function mount(component: HTMLElement, view: View, state: any): void {
 }
 
 function update(): void {
-  _mount.innerHTML = toHtml(_view({ ..._state }));
+  _mount.innerHTML = html(_view({ ..._state }));
 }
 
 function doEvent(handlerKey: number): void {
-  _state = { ..._state, ...(_handlers.get(handlerKey)?.(_state) ?? {}) };
+  _state = { ..._state, ..._handlers.get(handlerKey)?.(_state) };
   update();
 }
 
-const toHtml = (node: any): string =>
-  isUnd(node) ? "" : isArr(node) ? arrayToHtml(node) : node.toString();
+const html = (x: any): string => (isArr(x) ? arrHtml(x) : x.toString());
 
-function arrayToHtml(node: any[]): string {
-  const [head] = node.splice(0, 1);
+function arrHtml(node: any[]): string {
+  const head = node.shift();
   if (isArr(head)) {
-    return arrayToHtml(head) + node.map(toHtml).join("");
+    return arrHtml(head) + node.map(html).join("");
   }
-  if (!isStr(head)) {
-    console.log(`Bad head: ${head}`);
-    return "";
-  }
-  let elAttrs = "";
-  if (node.length && isObj(node[0]) && !isArr(node[0])) {
-    elAttrs = attrs(node[0]);
-    node.shift();
-  }
+  let elAttrs =
+    node.length && isObj(node[0]) && !isArr(node[0])
+      ? attrs(node.shift()).join("")
+      : "";
   const [tagAndId, ...classes] = head.split(".");
   const [tag, id] = tagAndId.split("#");
   return `<${tag}${id ? ` id="${id}"` : ""} ${
     classes.length ? `class="${classes.join(" ")}` : ""
-  }${elAttrs}>
-            ${node.map(toHtml).join("")}
-          </${tag}>`;
+  }${elAttrs}>${node.map(html).join("")}</${tag}>`;
 }
 
-const attr = (attr: string | DomEvent): string =>
-  isEvt(attr) ? makeEvent(attr) : attr;
-const attrs = (node: any): string =>
-  Object.keys(node)
-    .map(a => ` ${a}="${attr(node[a])}"`)
-    .join("");
+const attr = (a: any): string => (isEvt(a) ? makeEvt(a) : a);
+const attrs = (node: any): string[] =>
+  Object.keys(node).map(a => ` ${a}="${attr(node[a])}"`);
 
-function makeEvent(handler: DomEvent): string {
+function makeEvt(handler: DomEvent): string {
   const key = hash(handler.toString());
   _handlers.set(key, handler);
   return `doEvent(${key})`;
