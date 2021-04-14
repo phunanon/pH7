ec6efd40f621cfc9eb482c0876bcdc76a916c636
Patrick Bowen <phunanon@users.noreply.github.com>
2021-04-14 10:20:23 +0100

    Another shrink

--- pH7.ts
+++ pH7.ts
@@ -1,18 +1,11 @@
 type DomEvent = (state: any) => any;
 type View = (state: any) => any[];
 
-const isArr = Array.isArray;
-const isStr = (x: any): x is string => typeof x == "string";
-const isObj = (x: any): x is object => typeof x == "object";
+const isObj = (x: any): x is object =>
+  Object.prototype.toString.call(x) == "[object Object]";
 const isEvt = (x: any): x is DomEvent => typeof x == "function";
 
-function hash(s: string): number {
-  for (var i = 0, h = 9; i < s.length; )
-    h = Math.imul(h ^ s.charCodeAt(i++), 9 ** 9);
-  return h ^ (h >>> 9);
-}
-
-let _mount: HTMLElement, _view: View, _state: any;
+let _mount: HTMLElement, _view: View, _state: {};
 const _handlers = new Map<number, DomEvent>();
 
 function mount(component: HTMLElement, view: View, state: any): void {
@@ -21,6 +14,7 @@ function mount(component: HTMLElement, view: View, state: any): void {
 }
 
 function update(): void {
+  _handlers.clear();
   _mount.innerHTML = html(_view({ ..._state }));
 }
 
@@ -29,17 +23,14 @@ function doEvent(handlerKey: number): void {
   update();
 }
 
-const html = (x: any): string => (isArr(x) ? arrHtml(x) : x.toString());
+const html = (x: any): string => (Array.isArray(x) ? arrHtml(x) : x.toString());
 
-function arrHtml(node: any[]): string {
-  const head = node.shift();
-  if (isArr(head)) {
+function arrHtml([head, ...node]: any[]): string {
+  if (Array.isArray(head)) {
     return arrHtml(head) + node.map(html).join("");
   }
   let elAttrs =
-    node.length && isObj(node[0]) && !isArr(node[0])
-      ? attrs(node.shift()).join("")
-      : "";
+    node.length && isObj(node[0]) ? attrs(node.shift()).join("") : "";
   const [tagAndId, ...classes] = head.split(".");
   const [tag, id] = tagAndId.split("#");
   return `<${tag}${id ? ` id="${id}"` : ""} ${
@@ -52,7 +43,7 @@ const attrs = (node: any): string[] =>
   Object.keys(node).map(a => ` ${a}="${attr(node[a])}"`);
 
 function makeEvt(handler: DomEvent): string {
-  const key = hash(handler.toString());
+  const key = Math.random();
   _handlers.set(key, handler);
   return `doEvent(${key})`;
 }
